39dcb4eac0af4d52a1bf39379f2cf846e73543b3
vhphong <vhphong@gmail.com>
2022-12-04 20:08:53 -0500

    Add existing project files to Git

---
+++ src/main/java/com/phongvo/credentialsvalidation/CredentialsValidationApplication.java
@@ -0,0 +1,13 @@
+package com.phongvo.credentialsvalidation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CredentialsValidationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CredentialsValidationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/phongvo/credentialsvalidation/CredentialsValidationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phongvo.credentialsvalidation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CredentialsValidationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
